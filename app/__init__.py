4dd5584d96b9ccb5fafd9e85dd19afef1cd0eed4
Zachary Leonardo <leonardoz@aldenv169.allegheny.edu>
2019-04-18 13:52:43 -0400

    added login.py, wokrin gon index html

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,9 @@ import flask
 
 # main flask instance
 app = flask.Flask(__name__)
+
+from . import index
+from . import db_connect
+from . import login
+from . import students
+from . import teachers
