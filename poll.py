7a6930610d00cbe0e7fcccb51bb3490114c34047
Taylor Lyle <lylet@allegheny.edu>
2019-04-21 13:21:50 -0400

    push before rewriting local files

--- poll.py
+++ poll.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
 
-filename = "data.txt"
-
 poll_data = {
-   'question' : 'What is your favorite color?',
-   'fields'   : ['Blue', 'Yellow', 'Pink', 'Red', 'Sky Blue', 'Purple']
+   'question' : 'Which web framework do you use?',
+   'fields'   : ['Flask', 'Django', 'TurboGears', 'web2py', 'pylonsproject']
 }
+filename = 'data.txt'
 
 @app.route('/')
 def root():
@@ -23,9 +22,30 @@ def poll():
 
     return render_template('thankyou.html', data=poll_data)
 
-@app.route('/long_answer')
-def long_answer():
-	
+@app.route('/results')
+def show_results():
+    votes = {}
+    for f in poll_data['fields']:
+        votes[f] = 0
+
+    f  = open(filename, 'r')
+    for line in f:
+        vote = line.rstrip("\n")
+        votes[vote] += 1
+
+    return render_template('results.html', data=poll_data, votes=votes)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+"""@app.route('/long_answer')
+def long_answer():
+    response = request.args.get('field')
+
+    out = open(filename, 'a')
+    out.write( vote + '\n' )
+    out.close()
+
+    return render_template('long_answer.html', data=poll_data)"""
