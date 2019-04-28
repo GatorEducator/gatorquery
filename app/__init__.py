41a1c9d2560fd8ff4a5a1d83684b58f5c00b684e
Dillon Thoma <thomad@aldenv100.allegheny.edu>
2019-04-28 13:14:03 -0400

    Moving imports to top of the file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 """ Application package initializer """
 import flask
-
-# main flask instance
-app = flask.Flask(__name__)
-
 from . import index
 from . import db_connect
 from . import login
 from . import students
 from . import teachers
+
+# main flask instance
+app = flask.Flask(__name__)
