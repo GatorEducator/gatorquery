55a3d057ed780f2c17e18f6ba15be2e014288461
Zachary Leonardo <leonardoz@aldenv101.allegheny.edu>
2019-05-01 12:21:48 -0400

    revised db teardown

--- app/db_connect.py
+++ app/db_connect.py
@@ -4,28 +4,23 @@ import functools
 import sqlite3
 import flask
 
-# from flask import g, session, escape
-from app import app
-
 
 DATABASE = "database.db"
 
 
 def get_db():
     """ Retrieve the database """
-    db = getattr(flask.g, "_database", None)
-    if db is None:
-        db = flask.g_database = sqlite3.connect(DATABASE)
+    db = sqlite3.connect(DATABASE)
     return db
 
 
 @app.teardown_appcontext
 # pylint: disable=unused-argument
-def close_connection(exception):
+def close_connection(exception=None):
     """ Closes the connection to the database """
-    db = getattr(flask.g, "_database", None)
-    if db is not None:
-        db.close()
+    if exception is not None:
+        print(f"ERROR: {exception}")
+    db.close()
 
 
 def query_db(query, args=(), one=False):
