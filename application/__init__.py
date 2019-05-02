34d2a65c191a682ea0689586e3ae7b3047ffcf54
Zachary Leonardo <leonardoz@aldenv100.allegheny.edu>
2019-05-02 12:05:08 -0400

    black and flake 8 fixes

--- application/__init__.py
+++ application/__init__.py
@@ -7,9 +7,12 @@ app = flask.Flask(__name__)
 
 # pylint: disable=wrong-import-position
 from . import login
+
 # pylint: disable=wrong-import-position
 from . import poll
+
 # pylint: disable=wrong-import-position
 from . import create
+
 # pylint: disable=wrong-import-position
 from . import home
